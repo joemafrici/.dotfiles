f874e3b75a502457c97f59eb7f39212619a9cb19
Joe Mafrici <hello@joemafrici.com>
2022-04-18 13:18:50 -0700

    Add command to default vi mode in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,12 @@ end
 # Remove need for . in front of executables
 set PATH .:$PATH
 export PATH
-# Test comment
+
+# Compiler flags
 export CFLAGS="-g -Wall -O0"
 export CXXFLAGS="-g -Wall -O0"
+
+# config alias for dotfile management
 alias config='/usr/bin/git --git-dir=/home/joe/.cfg/ --work-tree=/home/joe'
+
+fish_vi_key_bindings
