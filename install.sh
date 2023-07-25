fa4c789e48037085ac184a7b48da7d39dddadf5b
Joe Mafrici <hello@joemafrici.com>
2023-07-25 14:22:25 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd ~/
+
+# Install zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# Install dev tools
+sudo apt-get update
+sudo apt-get install build-essential
+
+# Launch zsh
+zsh
+# Install Neovim Prerequisites
+sudo apt-get install ninja-build gettext cmake unzip curl
+
+cd ~/
+
+# Install Neovim
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+nvim
+# Install Packer
+# might not need to do this 
+# see: https://github.com/wbthomason/packer.nvim#bootstrapping
+#git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+# ~/.local/share/nvim/site/pack/packer/start/packer.nvim
