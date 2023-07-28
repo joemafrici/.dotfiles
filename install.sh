ce6592c4dbfbaca4b3ba6cf4e6d354b4509a3d37
Joe Mafrici <hello@joemafrici.com>
2023-07-27 21:51:40 -0700

    cd home

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ cd ~/
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+cd ~/
 
 # Install Packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
