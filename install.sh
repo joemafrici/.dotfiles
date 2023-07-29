e54ce8ad7202583adc80076379c3436951bdab33
Joe Mafrici <hello@joemafrici.com>
2023-07-28 21:41:23 -0700

    golang install seems to be working

--- install.sh
+++ install.sh
@@ -39,22 +39,24 @@ cd ~/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# Instal Go
+# Install Go
 # Define the download URL for the latest stable Go version for Linux AMD64
-#!/bin/bash
 
 # Define the download URL for the latest stable Go version for Linux AMD64
 DOWNLOAD_URL="https://go.dev/$(curl -s https://go.dev/dl/ | grep -o -m 1 'dl/go[0-9.]*.linux-amd64.tar.gz')"
 echo $DOWNLOAD_URL
 
 # Extract the version number from the download URL
-VERSION=$(echo "$DOWNLOAD_URL" | grep -o -m 1 'go[0-9.]*.linux-amd64.tar.gz' | cut -c 3- | rev | cut -c 16- | rev)
+VERSION=$(echo "$DOWNLOAD_URL" | grep -o -m 1 'go[0-9.]*.linux-amd64.tar.gz' | cut -c 3- | rev | cut -c 20- | rev)
 echo $VERSION
+echo "go$VERSION.linux-amd64.tar.gz"
 
 # Download the Go package
 wget "$DOWNLOAD_URL"
 
 # Install Go
+# need to add checking for existing go directory
+# and removing first if it exists
 sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
 rm go$VERSION.linux-amd64.tar.gz
 
