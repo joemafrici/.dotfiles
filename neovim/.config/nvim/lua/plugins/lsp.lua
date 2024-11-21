78f9c85567bb6009463ec7311c1c89e2940d02b9
Joe Mafrici <hi@gojoe.dev>
2024-11-21 10:46:59 -0800

    add pyright

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -145,11 +145,10 @@ end
 local servers = {
 	clangd = {},
 	gopls = {},
-	-- pyright = {},
+	pyright = {},
 	rust_analyzer = {},
 	tailwindcss = {},
 	solargraph = {},
-	pylsp = {},
 	-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 	--
 	-- Some languages (like typescript) have entire language plugins that can be useful:
